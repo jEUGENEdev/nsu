6fb75abbe23dbe04c6684a4259415c484982b913
Eugene <genyamateyuk@gmail.com>
2022-09-19 06:29:10 +0300

    refactoring task 2

--- C/work3/main.c
+++ C/work3/main.c
@@ -3,20 +3,45 @@
 #include <assert.h>
 #include <math.h>
 
+/* START TASK 0 */
+
 char isPrime(unsigned long long x) {
     assert(x >= 2);
-    for(unsigned long long i = 2; i <= sqrt((double) x); i++)
+    for(unsigned long long i = 2; i <= (unsigned long long) sqrt((double) x); i++)
         if(x % i == 0) {
             return 0;
         }
     return 1;
 }
 
-int main() {
+void task0() {
     unsigned long long N;
     scanf("%lld", &N);
     for(unsigned long long i = 2; i <= N; i++)
         if(isPrime(i))
-            printf("%lld", N);
+            printf("%lld\n", i);
+}
+
+/* END TASK 0 */
+
+/* START TASK 1 */
+
+int countDivs(int x) {
+    assert(x > 0);
+    int count = 0;
+    for(int i = 1; i <= (int) sqrt(x); i++)
+        if(x % i == 0)
+            count += 2;
+    return count;
+}
+
+void task1() {
+    printf("%d", countDivs(100));
+}
+
+/* END TASK 1 */
+
+int main() {
+    task1();
     return 0;
 }

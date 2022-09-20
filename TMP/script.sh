45d54aed5a925c7412431026b3410432e82e789a
Eugene <genyamateyuk@gmail.com>
2022-09-20 11:14:38 +0600

    tmp

--- TMP/script.sh
+++ TMP/script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
 #args
 echo "$0 $1 $2 $3 ok"
+
 
 #cycle
 for id in {1..5}
@@ -9,12 +11,16 @@ do
 	echo hello $id
 done
 
+
 #values
 WHAT=`cat /home/administrator/nsu/TMP/file.txt`
+value="value"
 echo $WHAT
+echo $value
+
 
 #condition
-if [1 = 1]
+if [ 1 > 0 ]
 then
 	echo this 1
 else

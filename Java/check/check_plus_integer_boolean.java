3a852c2cd79fdc2a6c673b0040ca8fb395c50806
Eugene <genyamateyuk@gmail.com>
2022-09-25 23:46:43 +0600

    xz

---
+++ Java/check/check_plus_integer_boolean.java
@@ -0,0 +1,11 @@
+//This fact was noticed in an Internet joke, and it seemed interesting to me
+
+public class check_plus_integer_boolean {
+        public static void main(String[] args) {
+                /**
+                * not compile
+                * System.out.print(true + 2);
+                */
+        }
+
+}

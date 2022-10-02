0a06ca9b2111b5f5bbb1a3d5c488afd5e56fdfba
Eugene <genyamateyuk@gmail.com>
2022-10-02 22:24:01 +0300

    dz7 C update

--- C/work7/main.c
+++ C/work7/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "../work5/funcarr.c"
 
-int main() {
+void task1() {
     int N;
     scanf("%d", &N);
     int* b = malloc(sizeof(int) * N);
@@ -17,5 +17,41 @@ int main() {
     printf("%d\n", findInArr(b, N, 3));
     extractDigits(b, N, 1234);
     printf("%d", compareArrays(b, b, N, N));
+    free(b);
+}
+
+void copy(int* arr, int* cp, size_t size) {
+    for(size_t i = 0; i < size; i++) {
+        cp[i] = arr[i];
+    }
+}
+
+// Allocates a little bit of memory
+void pickUp(int* arr, size_t size) {
+    int* g = malloc(sizeof(int) * (size - 1));
+    copy(arr, g, size - 1);
+    free(arr);
+    arr = malloc(sizeof(int) * size);
+    copy(g, arr, size - 1);
+    free(g);
+}
+
+void task2() {
+    int n;
+    scanf("%d", &n);
+    size_t size = 0;
+    int* m = malloc(sizeof(int));
+    while(n != 0) {
+        size++;
+        pickUp(m, size);
+        m[size - 1] = n;
+        scanf("%d", &n);
+    }
+    printArr(m, size);
+}
+
+int main() {
+//    task1();
+    task2();
     return 0;
 }

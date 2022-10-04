49d7e38d474042c5e37500d5d6713fa411a2ddb5
Eugene <genyamateyuk@gmail.com>
2022-10-04 07:40:28 +0300

    work8 scanArr() C

--- C/work8/main.c
+++ C/work8/main.c
@@ -45,8 +45,28 @@ void removeSubArr(int* arr, int* remove, size_t size1, size_t size2, size_t* new
     *newSize = size1;
 }
 
+// scanArr already exists in funcarr.c
+void arrScan(int* arr, size_t* size, size_t* capacity) {
+    int n = -1;
+    *size = 1, *capacity = 1;
+    scanf("%d", &n);
+    arr = malloc(sizeof(int));
+    arr[0] = n;
+    while(n != 0) {
+        scanf("%d", &n);
+        if(size >= capacity) {
+            arr = realloc(arr, sizeof(int) * *size * 2);
+            notNull(arr);
+            *capacity = *size * 2;
+        }
+        *size = *size + 1;
+        arr[*size - 1] = n;
+    }
+    printf("%d\n", arr[0]);
+}
+
 int main() {
-    int array1[] = {0, 1, 2, 3, 1, 2, 4}, array2[] = {1, 2};
+//    int array1[] = {0, 1, 2, 3, 1, 2, 4}, array2[] = {1, 2};
 //    int* concatArr;
 //    size_t concatArrSize;
 //    concat(fastDymArrInit(array1, 6), fastDymArrInit(array2, 4), 6, 4, &concatArr, &concatArrSize);
@@ -60,4 +80,10 @@ int main() {
 //    removeSubArr(arr, arr1, 7, 2, &newSize);
 //    printf("%d\n", (int) newSize);
 //    printArr(arr, newSize);
+
+    int* arr;
+    size_t size, capacity;
+    arrScan(arr, &size, &capacity);
+    printf("%d / %d", (int) size, (int) capacity);
+    printArr(arr, size);
 }

42e957dc5570705c6a60388dbc4ffd720d1683db
Eugene <genyamateyuk@gmail.com>
2022-10-05 15:50:15 +0300

    work1 my_zip() Python

--- Python/work1/main.py
+++ Python/work1/main.py
@@ -9,6 +9,19 @@ def my_bin(n):
     return count
 
 
-print("7 -> 3", my_bin(7) == 3)  # True
-print("10 -> 2", my_bin(10) == 2)  # True
-print("-123 -> 3", my_bin(-123) == 3)  # True
+# print("7 -> 3", my_bin(7) == 3)  # True
+# print("10 -> 2", my_bin(10) == 2)  # True
+# print("-123 -> 3", my_bin(-123) == 3)  # True
+
+
+def my_zip(list1: list, list2: list):
+    mn = min(len(list1), len(list2))
+    res = []
+    for i in range(mn):
+        res += [(list1[i], list2[i])]
+    return res
+
+
+print("[1, 2, 3], ['a', 'b'] -> [(1, 'a'), (2, 'b')]", my_zip([1, 2, 3], ['a', 'b']) == [(1, 'a'), (2, 'b')])
+print("[30, 1, 2, 3], ['a', 'b', 'x', 'c'] -> [(30, 'a'), (1, 'b'), (2, 'x'), (3, 'c')]",
+      my_zip([30, 1, 2, 3], ['a', 'b', 'x', 'c']) == [(30, 'a'), (1, 'b'), (2, 'x'), (3, 'c')])

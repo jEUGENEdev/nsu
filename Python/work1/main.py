4360e8c7347f7bddcdc412022845febc789e7c90
Eugene <genyamateyuk@gmail.com>
2022-10-09 13:06:06 +0300

    Python work1: more fixed my_bin()

--- Python/work1/main.py
+++ Python/work1/main.py
@@ -1,15 +1,17 @@
 # TASK1
 def my_bin(n):
-    count = 1 if n < 0 else 0
+    count = n & 1
+    n >>= 1
     while n not in [0, -1]:
         count += n & 1
         n >>= 1
-    return count
+    return count + (n & 1)
 
 
 print("7 -> 3", my_bin(7) == 3)  # True
 print("10 -> 2", my_bin(10) == 2)  # True
 print("-123 -> 3", my_bin(-123) == 3, my_bin(-123))  # True
+print(my_bin(-1))
 
 
 # TASK2
@@ -29,6 +31,5 @@ def my_zip(list1: list, list2: list):
 # massive1 | massive2 | massive3
 def my_matrix_builder(s: str):
     return list(map(lambda x: [y for y in map(float, x.split())], s.split('|')))
-
 
 # print(my_matrix_builder('1 2 | 3 4')[0][1])

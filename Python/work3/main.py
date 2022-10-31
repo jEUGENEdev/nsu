f13d092d98d9b13467a92d2fa3a2fb86d33de129
Eugene <genyamateyuk@gmail.com>
2022-10-31 20:28:32 +0700

    python: work3 update

--- Python/work3/main.py
+++ Python/work3/main.py
@@ -1,23 +1,21 @@
-def expand_element_list(_list, cur_depth, depth, res, not_all=True):
-    if (cur_depth >= depth) and not_all:
+def expand_element_list(_list, depth, res, not_all=True):
+    if (depth <= 0) and not_all:
         res += _list
         return res
     for el in _list:
         if isinstance(el, list):
-            expand_element_list(el, cur_depth + 1, depth, res, not_all)
+            expand_element_list(el, depth - 1, res, not_all)
         else:
             res.append(el)
     return res
 
 
 def flatten(_list):
-    return expand_element_list(_list, 0, 0, [], False)
-
-# And where is polymorphism?
+    return expand_element_list(_list, 0, [], False)
 
 
 def flatten1(_list, depth):
-    return expand_element_list(_list, 1, depth, [])
+    return expand_element_list(_list, depth, [])
 
 
 print(flatten([1, 2, [4, 5], [6, [7]], 8]))

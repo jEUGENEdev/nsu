db301956d649392461c2da23d9121530df9cda87
Eugene <genyamateyuk@gmail.com>
2022-11-22 13:11:24 +0700

    without time

--- Python/work6/main.py
+++ Python/work6/main.py
@@ -1,30 +1,20 @@
-from time import time
-
-
 class LRUCache:
     def __init__(self, capacity=16):
-        self.timestamps = dict()
+        self.timestamps_queue = []
         self.data = dict()
         self.capacity = capacity
 
     def __put(self, key, value):
         self.data[key] = value
-        self.timestamps[key] = time()
-
-    def __get_key_by_min_value(self):
-        mn = ('key', 0)
-        for key, value in self.timestamps.items():
-            if value < mn[1] or mn[1] == 0:
-                mn = key, value
-        return mn[0]
+        self.timestamps_queue.append((key, value))
 
     def put(self, key, value):
         if len(self.data) + 1 <= self.capacity:
             self.__put(key, value)
         else:
-            min_key = self.__get_key_by_min_value()
+            min_key = self.timestamps_queue[0][0]
+            del self.timestamps_queue[0]
             del self.data[min_key]
-            del self.timestamps[min_key]
             self.__put(key, value)
 
     def get(self, key):
